125dd6d9a456da263f39c97e14443f7b897eb7a4
Camden Narzt <camden.narzt@hotmail.com>
2017-05-29 12:44:32 -0600

    cleanup impl

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,18 @@ fn main() {
     let stdin = io::stdin();
     let stdout = io::stdout();
     let mut args = env::args().skip(1).peekable();//skip executable name
+    let name:Option<path::PathBuf> = args.peek().map(path::PathBuf::from);
 
-    let mut f = args.peek().map_or(None,|string|{
-    	if path::Path::new(&string).is_file(){
-	    fs::OpenOptions::new().read(true).write(true).create(true).open(string).ok()
-	}else{
-	    None
-	}
+    let mut f = name.as_ref().and_then(|path| if path.is_file() {
+	fs::File::open(path).ok()
+    }else{
+	None
     });
     
-    let input : Box<io::BufRead> = match f.as_mut() {
-        Some(&mut ref f) => {
+    let input : Box<io::BufRead> = match f {
+        Some(f) => {
 	    args.next();//skip file name
-            Box::new(io::BufReader::new(f.try_clone().unwrap()))
+            Box::new(io::BufReader::new(f))
         },
         None => Box::new(stdin.lock())
     };
@@ -28,11 +27,14 @@ fn main() {
     let line_numbers: Vec<usize> = args.map(|s|s.parse::<usize>().expect("encountered a non integer argument")-1).collect();
     let contents:Vec<(usize,io::Result<String>)> = input.lines().enumerate().filter(|&(i,_)|!line_numbers.contains(&i)).collect();
 
-    let mut output: Box<io::Write> = match f.as_mut() {
-        Some(f) => {
-            f.set_len(0).expect("failed to truncate file, aborting");
-            Box::new(io::BufWriter::new(f.try_clone().unwrap()))
-        },
+    f = name.as_ref().and_then(|path| if path.is_file() {
+        fs::File::create(path).ok()
+    }else{
+	None
+    });
+
+    let mut output: Box<io::Write> = match f {
+        Some(f) => Box::new(io::BufWriter::new(f)),
         None => Box::new(stdout.lock())
     };
 
